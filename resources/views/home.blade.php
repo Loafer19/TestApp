e176667b3499fe667b9b14b20e44f7b6e330861e
Loafer18 <platonpopelishin@gmail.com>
2019-01-18 02:10:46 +0200

    update 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,15 @@
 @extends('layouts.master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('pageName', 'Dashboard')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="/home">Home</a></li>
+@endsection
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('mainContent')
+    Table
+@endsection
+
+@section('scripts')
+    
 @endsection
